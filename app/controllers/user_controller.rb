c77ce4b775f5f491f561066a5a10a5c7c0f55dc1
Millertron <senjinkoha@gmail.com>
2019-10-02 13:10:51 +0100

    Implemented basic password reset

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,8 +2,18 @@ class UserController < ApplicationController
   before_action :authorize
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if (@user.status_registered?)
+      @user.status_active!
+      puts params[:user][:password]
+      @user.password = params[:user][:password]
+      @user.save
+    end
+    redirect_to root_url
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
   resources :sessions, only: [:new, :create, :destroy]
+  resources :user, only: [:edit, :update]
 
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  get 'user/edit'
-  post 'user/update'
 
 end
